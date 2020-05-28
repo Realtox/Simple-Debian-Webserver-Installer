5e361b82f1a30c89667b18c5822a3eab8d32d32b
Realtox <39673627+Realtox@users.noreply.github.com>
2020-05-28 17:11:42 +0200

    Updating Update.sh

--- installer.sh
+++ installer.sh
@@ -48,7 +48,7 @@ read ANS
 if [ "$ANS" == "Y" ]
     then
         echo "Starting"
-		sudo apt-get install php7.3-cli php7.3-fpm php7.3-json php7.3-pdo php7.3-mysql php7.3-zip php7.3-gd php7.3-mbstring php7.3-curl php7.3-xml php7.3-bcmath php7.3-json
+		apt-get install php7.3-cli php7.3-fpm php7.3-json php7.3-pdo php7.3-mysql php7.3-zip php7.3-gd php7.3-mbstring php7.3-curl php7.3-xml php7.3-bcmath php7.3-json
     else
         echo "Next"
 	exit
